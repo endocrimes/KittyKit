7b0f55f609e805c8423ca6957afb754f43b00aa7
Daniel Tomlinson <Dan@Tomlinson.io>
2015-11-09 16:27:51 -0500

    Add APIClientProtocol

---
+++ KittyKit/APIClient.swift
@@ -0,0 +1,26 @@
+//
+//  APIClient.swift
+//  KittyKit
+//
+//  Created by Daniel Tomlinson on 09/11/2015.
+//  Copyright © 2015 Rocket Apps Limited. All rights reserved.
+//
+
+import Foundation
+
+public typealias AuthenticityToken = String
+
+public protocol APIClientProtocol {
+    func fetchAuthenticityToken(completion: Maybe<AuthenticityToken, ErrorType> -> ())
+    func submitURL(url: String, token: AuthenticityToken, completion: Maybe<String, ErrorType> -> ())
+}
+
+public class APIClient: APIClientProtocol {
+    public func fetchAuthenticityToken(completion: Maybe<AuthenticityToken, ErrorType> -> ()) {
+        
+    }
+    
+    public func submitURL(url: String, token: AuthenticityToken, completion: Maybe<String, ErrorType> -> ()) {
+        
+    }
+}
