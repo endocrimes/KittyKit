20ff3e7e9669304b1cd58fb6dccac34381d29341
Danielle Lancashire <dani@builds.terrible.systems>
2015-11-09 16:05:23 -0500

    Initial Commit

---
+++ KittyKit/KittyKit.h
@@ -0,0 +1,19 @@
+//
+//  KittyKit.h
+//  KittyKit
+//
+//  Created by  Danielle Lancashireon 09/11/2015.
+//  Copyright © 2015 Rocket Apps Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KittyKit.
+FOUNDATION_EXPORT double KittyKitVersionNumber;
+
+//! Project version string for KittyKit.
+FOUNDATION_EXPORT const unsigned char KittyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KittyKit/PublicHeader.h>
+
+
