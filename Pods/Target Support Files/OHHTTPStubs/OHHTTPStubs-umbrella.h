ec4e0e992649be9b46682da47cf1f06aa886b070
Daniel Tomlinson <Dan@Tomlinson.io>
2015-11-09 17:03:37 -0500

    Pod install

---
+++ Pods/Target Support Files/OHHTTPStubs/OHHTTPStubs-umbrella.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+
+#import "Compatibility.h"
+#import "OHHTTPStubs.h"
+#import "OHHTTPStubsResponse.h"
+
+FOUNDATION_EXPORT double OHHTTPStubsVersionNumber;
+FOUNDATION_EXPORT const unsigned char OHHTTPStubsVersionString[];
+
