e3beb94a98ea4fb23f18b6aa6a15db70cc637ca3
Daniel Tomlinson <Dan@Tomlinson.io>
2015-11-10 13:57:49 -0500

    Remove DANCurlRequest

--- Pods/Target Support Files/DANCurlRequest/DANCurlRequest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DANCurlRequest : NSObject
-@end
-@implementation PodsDummy_DANCurlRequest
-@end
--- Pods/Target Support Files/Pods-KittyKit/Pods-KittyKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KittyKit : NSObject
-@end
-@implementation PodsDummy_Pods_KittyKit
-@end
