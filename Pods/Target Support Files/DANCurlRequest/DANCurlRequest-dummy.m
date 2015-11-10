cba239b876208c2f20102fd194ca87fba9eae435
Danielle Lancashire <dani@builds.terrible.systems>
2015-11-10 13:11:52 -0500

    Add DANCurlRequest

---
+++ Pods/Target Support Files/DANCurlRequest/DANCurlRequest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DANCurlRequest : NSObject
+@end
+@implementation PodsDummy_DANCurlRequest
+@end
---
+++ Pods/Target Support Files/DANCurlRequest/DANCurlRequest-umbrella.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+#import "DANCurlRequest.h"
+
+FOUNDATION_EXPORT double DANCurlRequestVersionNumber;
+FOUNDATION_EXPORT const unsigned char DANCurlRequestVersionString[];
+
