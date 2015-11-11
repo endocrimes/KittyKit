f65c70ab207af401d5ded8f077bda980315cfc3b
Alexsander Akers <me@a2.io>
2015-11-10 20:37:12 -0500

    Add publicly accessibile initializer

--- KittyKit/APIClient.swift
+++ KittyKit/APIClient.swift
@@ -28,6 +28,8 @@ public protocol APIClientProtocol {
 }
 
 public class APIClient: APIClientProtocol {
+    public init() {}
+
     public func fetchAuthenticityToken(completion: Either<AuthenticityToken, APIErrors> -> ()) {
         NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "https://small.cat")!) { data, response, error in
             guard let data = data, text = String(data: data, encoding: NSUTF8StringEncoding) else {
